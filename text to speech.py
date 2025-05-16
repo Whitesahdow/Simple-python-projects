638bd87165a88fb9da101c50b36351b357cc3768
Beka Endale <bekaendale116@gmail.com>
2025-05-16 21:07:52 +0300

    fourth commit

--- text to speech.py
+++ text to speech.py
@@ -5,7 +5,7 @@ user_input = input("Please enter a text so we can read it: ")
 
 # Check if the input is only digits
 if user_input.strip().isdigit():
-    engine.say("You did not enter a proper  sentence or string.")
+    engine.say("You did not enter a proper sentence or string.")
 else:
     engine.say(user_input) 
 
